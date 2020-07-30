9f0868053b3345b185846d70bf1b19e9614fa8eb
paalg <georgina.paal@dpdgroup.com>
2020-07-30 13:42:04 +0200

    chore: initialize app

---
+++ src/main/java/com/emarsys/issutracker/duedatecalculator/DuedatecalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.emarsys.issutracker.duedatecalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuedatecalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuedatecalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emarsys/issutracker/duedatecalculator/DuedatecalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emarsys.issutracker.duedatecalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuedatecalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
